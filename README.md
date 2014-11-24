e2ac417f97585fbac8495c04748a2b3cc1282dd0
Nick Fisher <spadgos@gmail.com>
2014-11-24 19:17:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+## DNS Endpoint Pool
+
+Manage and load-balance a pool of service endpoints retrieved from a DNS lookup for a service discovery name.
+
+![Empty pool](https://farm4.staticflickr.com/3480/3192156375_ca9377272a_z.jpg)
+
+## Example
+
+Given a DNS setup like so:
+
+```shell
+$ dig +tcp +short SRV my.domain.example.com
+0 0 8000 first.example.com.
+0 0 8000 second.example.com.
+0 0 8000 third.example.com.
+```
+
+This library exposes those service endpoints via an interface which will load balance (using unweighted round-robin
+only), and apply a circuit breaker to remove failing endpoints from the pool temporarily. Endpoints will be
+automatically updated on a timer.
+
+```js
+var DNSEndpointPool = require('dns-endpoint-pool');
+
+var pool = new DNSEndpointPool('my.domain.example.com', 10000, 5, 10000);
+
+var endpointInfo = pool.getEndpoint();
+if (endpointInfo) {
+  sendRequest('http://' + endpointInfo.url, function (err) {
+    // let the pool know if the request was successful
+    endpointInfo.callback(err);
+  });
+}
+```
+
+If the usage of a endpoint is unsuccessful too many times within a window of time, it will be temporarily disabled.
+During this time, `getEndpoints()` will not return that endpoint. Afterwards, it will be returned to the pool for one
+single usage, and removed again. If that usage is successful, it is restored to the pool fully. If it is unsuccessful,
+it is disabled once again.
+
+## API
+
+### `new DNSEndpointPool(serviceDiscoveryName, ttl, maxFailures, resetTimeout)`
+
+Creates a new pool object.
+
+- `serviceDiscoveryName`: the domain name to get endpoint values from
+- `ttl`: the time (in ms) that the DNS lookup values are valid for. They will automatically be refreshed on this
+  interval.
+- `maxFailures`: how many failures from a single endpoint before it is removed from the pool
+- `resetTimeout`: the length of the window in which to record failures. Also the timeout before an endpoint will be
+  tried again.
+
+### `pool.getEndpoint()`
+
+Returns the next active `endpoint` from the pool, or `null` if none are available. If none are available, the pool will
+emit `'noEndpoints'`.
+
+### `endpoint.url`
+
+The endpoint url (without protocol) from the DNS lookup.
+
+### `endpoint.callback(err)`
+
+A callback which should be executed exactly once for each call to `getEndpoint`. If the `err` is truthy, this will count
+as a failure of the endpoint. If falsey, it marks the endpoint as successful and allows it to remain in the pool.
+
+## Events
+
+- If a request to update the endpoints fails, the pool will emit an `'error'` event. The endpoint pool will continue to
+  function, using the previously fetched endpoints.
+- If it is not possible to return any values from `getEndpoints()` (because all endpoints are disabled, for example), the
+  pool will emit `'noEndpoints'`.
+
+
+```js
+var pool = new DNSEndpointPool('my.domain.example.com', 10000, 5, 10000);
+
+pool.on('error', function (err) {
+  log('Could not fetch endpoints');
+});
+pool.on('noEndpoints', function () {
+  log('No endpoints available');
+})
+```
+
+## Photo credit
+
+[Photo](https://www.flickr.com/photos/perspective23/3192156375) of a scary pool by [perspective23](https://www.flickr.com/photos/perspective23/) used under [CC license](https://creativecommons.org/licenses/by-nc-nd/2.0/)
+
+## License
+
+MIT, SoundCloud.
---
+++ index.js
@@ -0,0 +1,133 @@
+var EndpointPool,
+    _      = require('underscore'),
+    dns    = require('dns'),
+    Events = require('events'),
+    util   = require('util'),
+
+    CLOSED = 0,
+    HALF_OPEN_READY = 1,
+    HALF_OPEN_PENDING = 2,
+    OPEN = 3;
+
+module.exports = EndpointPool = function (discoveryName, ttl, maxFailures, resetTimeout) {
+  if (!discoveryName || !ttl || !maxFailures || !resetTimeout) {
+    throw new Error('Must supply all arguments');
+  }
+  Events.EventEmitter.call(this);
+
+  this.discoveryName = discoveryName;
+  this.ttl = ttl;
+  this.endpoints = [];
+  this._endpointOffset = 0;
+  this.maxFailures = maxFailures;
+  this.resetTimeout = resetTimeout;
+  this.update();
+};
+
+util.inherits(EndpointPool, Events.EventEmitter);
+
+_.extend(EndpointPool.prototype, {
+  update: function () {
+    dns.resolveSrv(this.discoveryName, function (err, endpoints) {
+      if (err) {
+        this.emit('error', err);
+      } else {
+        // endpoints = [/*endpoints[0],*/ {name: 'localhost', port: 1337 }];
+        this.setEndpoints(endpoints);
+      }
+      setTimeout(this.update.bind(this), this.ttl);
+    }.bind(this));
+  },
+
+  getEndpoint: function () {
+    var endpoint, i, l, offset;
+    for (i = 1, l = this.endpoints.length; i <= l; ++i) {
+      offset = (this._endpointOffset + i) % l;
+      endpoint = this.endpoints[offset];
+
+      switch (endpoint.state) {
+        case HALF_OPEN_READY:
+          endpoint.state = HALF_OPEN_PENDING; // let one through, then turn it off again
+          /* falls through */
+        case CLOSED:
+          this._endpointOffset = offset;
+          return endpoint;
+        // case OPEN: case HALF_OPEN_PENDING: // continue to the next one
+      }
+    }
+    this.emit('noEndpoints');
+    return null;
+  },
+
+  setEndpoints: function (endpoints) {
+    var newEndpoints, i, matchingEndpoint;
+
+    newEndpoints = endpoints.map(function (info) {
+      return new Endpoint(info, this.maxFailures, this.resetTimeout);
+    }, this);
+
+    for (i = this.endpoints.length; i--;) {
+      matchingEndpoint = _.findWhere(newEndpoints, { url: this.endpoints[i].url });
+
+      if (matchingEndpoint) { // found a match, remove it from `newEndpoints`, since it's not new
+        newEndpoints = _.without(newEndpoints, matchingEndpoint);
+      } else { // didn't find a match in endpoints, so kill that endpoint
+        this.endpoints.splice(i, 1);
+      }
+    }
+    // push all the actually-new endpoints in
+    this.endpoints.push.apply(this.endpoints, newEndpoints);
+  }
+});
+
+function Endpoint(info, maxFailures, resetTimeout) {
+  this.name = info.name;
+  this.port = info.port;
+  this.url = info.name + ':' + info.port;
+  this.state = CLOSED;
+  this.failCount = 0;
+  this.callback = endpointCallback.bind(this);
+  this.disable = disableEndpoint.bind(this);
+  this.resetTimeout = resetTimeout;
+  this.maxFailures = maxFailures;
+}
+
+function endpointCallback(err) {
+  if (err) {
+    if (this.state === OPEN) {
+      return;
+    }
+    // failed while half open
+    if (this.state === HALF_OPEN_PENDING) {
+      this.disable();
+      return;
+    }
+
+    // first failure, set up a window
+    if (this.failCount === 0) {
+      this._timeout = setTimeout(function () {
+        this.failCount = 0;
+      }.bind(this), this.resetTimeout);
+    }
+
+    this.failCount++;
+
+    if (this.failCount >= this.maxFailures) {
+      this.disable();
+    }
+  } else {
+    clearInterval(this._timeout);
+    clearInterval(this._reopenTimeout);
+    this.failCount = 0;
+    this.state = CLOSED;
+  }
+}
+
+function disableEndpoint() {
+  this.state = OPEN;
+  clearInterval(this._timeout);
+  clearInterval(this._reopenTimeout);
+  this._reopenTimeout = setTimeout(function () {
+    this.state = HALF_OPEN_READY;
+  }.bind(this), this.resetTimeout);
+}
